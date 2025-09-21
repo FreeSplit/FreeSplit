28a67601028b17b7046c9403920df05a6be1baef
Kristopher Sousa <kris.sousa@textnow.com>
2025-09-21 14:36:18 -0700

    CORS error

--- backend/rest_server.go
+++ backend/rest_server.go
@@ -38,7 +38,7 @@ func main() {
 		return func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-			w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Cache-Control, Pragma, Expires")
 
 			if r.Method == "OPTIONS" {
 				w.WriteHeader(http.StatusOK)
