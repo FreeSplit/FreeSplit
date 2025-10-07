cd79c32b61de664c575ccbd7deb841f30a0c012a
Thomas Forsyth <tomoblob@Thomass-MacBook-Pro.local>
2025-10-06 19:19:02 -0700

    add-feedback-cta-to-home

--- frontend/src/nav/sig-footer.tsx
+++ frontend/src/nav/sig-footer.tsx
@@ -4,7 +4,14 @@ const SigFooter: React.FC = () => {
 
   return (
     <footer>
-      <p className="p2">Created by <a href="https://thomasforsyth.design">Thomas</a> & <a href="https://www.linkedin.com/in/kmfsousa/">Kris</a></p>
+      <div className="v-flex align-center gap-8px">
+        <p className="p2">
+          🧱 Created by <a href="https://thomasforsyth.design" target="_blank" rel="noopener noreferrer">Thomas</a> & <a href="https://www.linkedin.com/in/kmfsousa/" target="_blank" rel="noopener noreferrer">Kris</a>.
+        </p> 
+        <p className="p2">💬 <a href="https://docs.google.com/forms/d/e/1FAIpQLSdWTIo2DnKLBNX2TBrNlJq4KB3tRBGf2EaRVS39hbe2NMBJXA/viewform?usp=header" target="_blank" rel="noopener noreferrer">
+          Submit feedback</a>.
+        </p>
+      </div>
     </footer>
   )
 }
